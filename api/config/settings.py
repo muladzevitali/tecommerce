210e99d28b2a4eb6eed398d393d1b96b40392027
muladzevitali <muladzevitali@gmail.com>
2022-05-26 23:11:11 +0400

    add static folder

--- api/config/settings.py
+++ api/config/settings.py
@@ -104,6 +104,6 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR.joinpath("static")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "user.User"
